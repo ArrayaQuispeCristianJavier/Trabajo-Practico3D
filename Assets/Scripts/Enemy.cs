038cf72b1f746c3ab4a631df851dcace275e010a
ArrayaQuispeCristianJavier <arrayacristian75@gmail.com>
2023-11-08 00:30:52 -0300

    El enemigo se mueve hacia la izquierda y derecha

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -7,21 +8,43 @@ public class Enemy : MonoBehaviour
     [SerializeField] float speedX = 3f;
     [SerializeField] float speedZ = 3f;
     [SerializeField] GameObject enemyBall;
+    //Me va a permitir trabajar con los enemigos deacuerdo a su Numero
+    [SerializeField] int enemyType;
+    [SerializeField] float speedXEnemy2 = 2f;
 
     private void Start()
     {   
         //Invoca al objeto (bala del enemigo cada 2 segundos)
         InvokeRepeating("ShootsBall",0, 1f);
+        ;
     }
     void Update()
     {
-     //Movimiento diagonal del enemigo
-    transform.Translate(speedX * Time.deltaTime, 0, speedZ * Time.deltaTime);
+     MoveEnemy();
     }
     public void ShootsBall()
     {
         //Se crea la bala del enemigo en la posición del enemigo
         Instantiate(enemyBall,transform.position,transform.rotation);
-        Debug.Log("El enemigo disparo");
+    }
+    public void MoveEnemy()
+    {
+        switch(enemyType)
+        {
+            case 1:
+            {
+             transform.Translate(speedX * Time.deltaTime, 0, speedZ * Time.deltaTime);
+             break;
+            }
+            case 2:
+            {
+            if (transform.position.x >= 4.44f || transform.position.x <= -4.44f)
+            {
+            speedXEnemy2 *= -1;           
+            }
+            transform.Translate(speedXEnemy2 * Time.deltaTime,0,0);
+            break;
+            }
+        }
     }
 }
