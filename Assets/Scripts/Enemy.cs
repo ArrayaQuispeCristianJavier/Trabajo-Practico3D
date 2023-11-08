0510baa1aa432ddfac6099424642bc3f2c4c74bb
ArrayaQuispeCristianJavier <arrayacristian75@gmail.com>
2023-11-07 23:31:41 -0300

    El enemigo realiza disparo y esta en prefabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,22 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float speedX = 3f;
     [SerializeField] float speedZ = 3f;
-    
+    [SerializeField] GameObject enemyBall;
+
+    private void Start()
+    {   
+        //Invoca al objeto (bala del enemigo cada 2 segundos)
+        InvokeRepeating("ShootsBall",0, 1f);
+    }
     void Update()
     {
      //Movimiento diagonal del enemigo
-    transform.Translate(speedX * Time.deltaTime, 0, -speedZ * Time.deltaTime);
+    transform.Translate(speedX * Time.deltaTime, 0, speedZ * Time.deltaTime);
+    }
+    public void ShootsBall()
+    {
+        //Se crea la bala del enemigo en la posición del enemigo
+        Instantiate(enemyBall,transform.position,transform.rotation);
+        Debug.Log("El enemigo disparo");
     }
 }
