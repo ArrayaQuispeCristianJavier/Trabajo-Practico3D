4152baadaa3f1d3929afac5f4b8072f798ae07a9
ArrayaQuispeCristianJavier <arrayacristian75@gmail.com>
2023-11-08 21:38:31 -0300

    El generador2 ahora choca hacia adelante y hacia atras del plano

--- Assets/Scripts/Generador.cs
+++ Assets/Scripts/Generador.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Generador : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
     [SerializeField] float initTime;
     [SerializeField] float repeatTime;
+    [SerializeField] byte typeGenerator;
+    [SerializeField] float speed = 3.0f;
+    
     void Start()
     {
-    InvokeRepeating("GenerateEnemy", initTime,repeatTime);   
-    }    
+        InvokeRepeating("GenerateEnemy", initTime, repeatTime);
+        StartCoroutine(MoveGeneratorCoroutine());
+    }
+
     public void GenerateEnemy()
     {
-    Instantiate(enemy, transform.position, transform.rotation);
+        Instantiate(enemy, transform.position, transform.rotation);
+    }
+
+    IEnumerator MoveGeneratorCoroutine()
+    {
+        float direction = 1; // 1 para mover hacia adelante, -1 para mover hacia atrás
+
+        do
+        {
+            if (typeGenerator == 2)
+            {
+                transform.Translate(0, 0, direction * speed * Time.deltaTime);
+
+                if (direction == 1 && transform.position.z >= 3.80f)
+                {
+                    direction = -1;
+                }
+                else if (direction == -1 && transform.position.z <= -3.18f)
+                {
+                    direction = 1;
+                }
+            }
+
+            yield return null;
+        } while (true);
     }
 }
+
