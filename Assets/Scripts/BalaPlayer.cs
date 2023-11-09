7f812a624899312c776eb070d1ac789c93d1754c
ArrayaQuispeCristianJavier <arrayacristian75@gmail.com>
2023-11-09 12:12:04 -0300

    Las balas del enemigo se destruye a llegar al limite del plano

--- Assets/Scripts/BalaPlayer.cs
+++ Assets/Scripts/BalaPlayer.cs
@@ -7,6 +7,15 @@ public class BalaPlayer : MonoBehaviour
     [SerializeField] float speedZ = 4f;
     void Update()
     {
-    transform.Translate(0, 0, speedZ * Time.deltaTime);
+        transform.Translate(0, 0, speedZ * Time.deltaTime);
+        DestroyBala();
+    }
+    //Metodo que destruirá la bala al cruzar el limite del plano de la bala enemigo
+    public void DestroyBala()
+    {
+        if (transform.position.z >= 5.68)
+        {
+            Destroy(gameObject);
+        }
     }
 }
